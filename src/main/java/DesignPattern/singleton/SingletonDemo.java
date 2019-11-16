85a4c09b39d1aabce0d6ee0ceb8344067423226d
gmf123 <894697547@qq.com>
2019-11-16 12:05:59 +0800

    设计模式

---
+++ src/main/java/DesignPattern/singleton/SingletonDemo.java
@@ -0,0 +1,16 @@
+package DesignPattern.singleton;
+
+/**
+ * @description:
+ * @author: gmf
+ * @date: Created in 2019/11/16 12:04
+ * @version: 枚举
+ * @modified By:
+ */
+public enum SingletonDemo {
+    INSTANCE;
+
+    public void SingletoDemo(){
+
+    }
+}
